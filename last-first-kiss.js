3f0e95038c2e458121ff5decdeb15e0e86ee65fb
loscutik <loscutik@gmail.com>
2023-05-08 16:07:56 +0000

    last-first-kiss

--- last-first-kiss.js
+++ last-first-kiss.js
@@ -10,7 +10,7 @@ kiss: that takes an array or string, and returns an array of 2 elements. The ret
 
 const first = (array) => array[0];
 const last = (array) => array[array.length - 1]
-const kiss = (array) => [array[0], array[array.length - 1]]
+const kiss = (array) => [array[array.length - 1], array[0]]
 
 console.log(first([1, 2, 3]));
 console.log(last([1, 2, 3]));
