cc880be4ffbc69fc84be210760bb5f6dd03e7ac9
loscutik <loscutik@gmail.com>
2023-05-08 15:24:33 +0000

    t3

--- mutability.js
+++ mutability.js
@@ -2,11 +2,11 @@
 // Increase the age of person by one, and set its country to 'FR'.
 // You must find a way to keep the original values of clone1 and clone2. The values of samePerson should change when person is changed.
 
-const person = {
-    name: 'Rick',
-    age: 77,
-    country: 'US',
-}
+// const person = {
+//     name: 'Rick',
+//     age: 77,
+//     country: 'US',
+// }
 
 const clone1 = Object.assign({}, person);
 const clone2 = Object.assign({}, person);
