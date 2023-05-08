5f6ea47946a7f6885f30e33c072885cd1141055f
loscutik <loscutik@gmail.com>
2023-05-08 15:46:56 +0000

    more or less

---
+++ more-or-less.js
@@ -0,0 +1,12 @@
+// Create 4 functions:
+
+// more that takes 1 argument and adds 1 to it.
+// less that takes 1 argument and subtracts 1 from it.
+// add that takes 2 arguments and adds them together.
+// sub that takes 2 arguments and subtracts the second argument from the first.
+// All the functions must return the result of the operations specified above.
+
+more = (n) => n++;
+less = (n) => n--;
+add = (n, m) => n + m;
+sub = (n, m) => n - m;
