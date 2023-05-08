82f871d80bc3d7a272842a192c6806b91314c516
loscutik <loscutik@gmail.com>
2023-05-08 16:20:19 +0000

    concat-str

--- concat-str.js
+++ concat-str.js
@@ -2,7 +2,7 @@
 Create a function named concatStr which takes 2 arguments and concatenates them.
 */
 
-const concatStr = (str1, str2) => str1 + str2;
+const concatStr = (str1, str2) => `${str1}${str2}`;
 
 
 console.log(concatStr('hello', 'world'));
