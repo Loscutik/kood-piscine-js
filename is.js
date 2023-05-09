ad13d580ae2bd336b635ed3adea8775c93b5d2e1
loscutik <loscutik@gmail.com>
2023-05-09 12:27:25 +0000

    is

--- is.js
+++ is.js
@@ -26,7 +26,7 @@ is.bool = (value) => typeof value === 'boolean';
 is.undef = (value) => typeof value === 'undefined';
 is.def = (value) => typeof value !== 'undefined';
 is.arr = (value) => Array.isArray(value);
-is.obj = (value) => Array.isArray(value); //typeof value === 'object';
+is.obj = (value) =>  value !== null && typeof value === 'object' && !Array.isArray(value);
 is.fun = (value) => typeof value === 'function';
 is.truthy = (value) => !!value;
 is.falsy = (value) => !value;
