f9056aa0fec2b2aa08436230f1a43bc10c32f9f5
loscutik <loscutik@gmail.com>
2023-05-11 16:15:08 +0000

    cut-corners

--- cut-corners.js
+++ cut-corners.js
@@ -12,37 +12,7 @@ No bitwise operator
 No % operator
 */
 
-const sign = (num) => {
-    if (num > 0) {
-        return 1;
-    }
-    if (num < 0) {
-        return -1;
-    }
-    return 0;
-}
 
-const sameSign = (a, b) => sign(a) === sign(b);
-
-const divide = (a, b) => {
-    if (b == 0) { return NaN; }
-    let a1 = a;
-    if (a < 0) {
-        a1 = -a;
-    }
-    let b1 = b;
-    if (b < 0) {
-        b1 = -b;
-    }
-    let res = -1;
-    do {
-        a1 -= b1;
-        res++;
-    } while (a1 >= 0)
-
-    if (sameSign(a, b)) return res;
-    else return -res;
-}
 
 // The largest integer smaller than or equal to x.
 const floor = (num) => {
@@ -53,16 +23,42 @@ const floor = (num) => {
     if (num === -Number.MIN_VALUE) return -1;
     if (Number.isInteger(num)) return num;
     let power = 0;
-    let num10=num
-    while (!Number.isInteger(num10)&&isFinite(num10)) {
-        num10 *= 10;
+    let tmp = num
+    // search for limits 2**n
+    while (tmp > 1 || tmp < -1) {
+        tmp /= 2;
         power++;
     }
-    // if (num10 === Infinity) {return 0}
-    // if (num10 === -Infinity) {return -0}
-    if(!isFinite(num10)){return 0}
-    const int = divide(num10, 10 ** power);
-    return num >= 0 ? int : int - 1;
+    let left;
+    let right;
+    if (num > 0) {
+         left = 2 ** (power - 1);
+         right = 2 ** (power);
+    } else {
+         left = -(2 ** (power));
+         right = -(2 ** (power - 1));
+    }
+    while (right - left > 1) {
+        let mid = (right + left) / 2;
+        if (num > mid) {
+            left = mid;
+        } else {
+            right = mid;
+        }
+    }
+
+
+
+
+    // while (!Number.isInteger(num10) && isFinite(num10)) {
+    //     num10 *= 10;
+    //     power++;
+    // }
+    // // if (num10 === Infinity) {return 0}
+    // // if (num10 === -Infinity) {return -0}
+    // if (!isFinite(num10)) { return 0 }
+    // const int = divide(num10, 10 ** power);
+    return left;//num >= 0 ? int : int - 1;
 }
 
 const ceil = (num) => {
@@ -84,13 +80,13 @@ const trunc = (num) => { return num >= 0 ? floor(num) : ceil(num); }
 
 
 /*
-const numss = [3.7, -3.7, 3.1, -3.1, 'e', Number.MIN_VALUE*(1.0000000+Number.MIN_VALUE),  Number.NEGATIVE_INFINITY, 2]
-floor(-Number.MIN_VALUE*10)
+const numss = [3.7, -3.7, 3.1, -3.1, 0xfffffffff + 0.1,Number.MIN_VALUE * (1.0000000 + Number.MIN_VALUE), Number.NEGATIVE_INFINITY, 2]
+//floor(-Number.MIN_VALUE * 10)
 console.log("round\n", numss.map(round))
 console.log("floor\n", numss.map(floor))
 console.log("trunc\n", numss.map(trunc))
 console.log("ceil\n", numss.map(ceil))
-console.log(Number.MIN_SAFE_INTEGER, Number.MAX_SAFE_INTEGER,Number.MIN_VALUE,Number.MAX_VALUE)
+console.log(0xfffffffff + 0.1, Number.MIN_SAFE_INTEGER, Number.MAX_SAFE_INTEGER, Number.MIN_VALUE, Number.MAX_VALUE)
 console.log(Number.POSITIVE_INFINITY + 1 === Infinity, Number.NEGATIVE_INFINITY + 1 === -Infinity)
   /*
 [ 4, -4, 3, -3 ]
