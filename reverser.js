354acc6fe5d2c3fd6d109a4fbc1c20354e5fa573
loscutik <loscutik@gmail.com>
2023-05-11 16:21:44 +0000

    reverser

--- reverser.js
+++ reverser.js
@@ -4,13 +4,14 @@ It should work like Array.reverse(), and of course you cannot use that.
 */
 
 const reverse = (arr) => {
-  if (typeof arr ==='string') {arr = arr.split('');} 
-    for (let i = 0; i < Math.floor(arr.length / 2); i++) {
-        let tmp = arr[i];
-        arr[i] = arr[arr.length - 1 - i];
-        arr[arr.length - 1 - i] = tmp;
-    }
-    return arr;
+  let array = true;
+  if (typeof arr === 'string') { arr = arr.split(''); array = false; }
+  for (let i = 0; i < Math.floor(arr.length / 2); i++) {
+    let tmp = arr[i];
+    arr[i] = arr[arr.length - 1 - i];
+    arr[arr.length - 1 - i] = tmp;
+  }
+  return array?arr:arr.join('');
 }
 
 const array1 = ['one', 'two', 'three'];
@@ -23,5 +24,4 @@ console.log('array1 after reverse:', array1);
 
 const str = 'hello';
 console.log('reversed array1:', reverse(str));
-// Expected output: "array1:" Array ["three", "two", "one"]
 console.log('array1 after reverse:', str);
