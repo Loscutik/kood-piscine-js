cf193bdc512f874942a28896f0cd969e43fdb4ee
loscutik <loscutik@gmail.com>
2023-05-16 16:12:23 +0000

    quantifiers

--- quantifiers.js
+++ quantifiers.js
@@ -25,7 +25,7 @@ function some(arr, func) {
     return false;
 }
 
-function every(arr, func) {
+function none(arr, func) {
     //return !arr.some(func);
     for (let i = 0; i < arr.length; i++) {
         if (func(arr[i],i,arr)) {
